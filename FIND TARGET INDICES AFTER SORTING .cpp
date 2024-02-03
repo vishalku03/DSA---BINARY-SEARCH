bf1ece7d5c338c8d13947344f154e2b7c258f075
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-03 22:41:45 +0530

    Add files via upload

---
+++ FIND TARGET INDICES AFTER SORTING .cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     vector<int> targetIndices(vector<int> &nums, int target)
+     {
+          sort(nums.begin(), nums.end());
+          vector<int> ans;
+
+          for (int i = 0; i < nums.size(); i++)
+          {
+               if (nums[i] == target)
+                    ans.push_back(i);
+          }
+          return ans;
+     }
+};
+
+// LEETCODE PROBLEM - 2089
